847535b635947b2565734587fea30229a1c43e4c
Makcollins <makcollins16@gmail.com>
2024-08-01 17:46:52 +0300

    payment inegration

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import authMiddleware from "../middleware/auth.js";
+import { placeorder } from '../controller/orderController.js';
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place",authMiddleware,placeorder);
+
+export default orderRouter;
