544ed837b9850dc63f5724fb9a4e5f1b4c7ec010
Collins Makui <52255997+Makcollins@users.noreply.github.com>
2024-09-22 08:52:07 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from './routes/orderRoute.js';
 
 // app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 // mongodb+srv://makcollins16:fb7BgPaSFFHrGf0z@fooddelivery.mqvogiz.mongodb.net/?
 
