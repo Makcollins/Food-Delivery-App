13ecc8532c8b6874f81032db5afb2dc3721a4e5f
Collins Makui <52255997+Makcollins@users.noreply.github.com>
2025-05-26 14:08:28 +0300

    Update WallPaper.jsx

--- client/src/components/WallPaper.jsx
+++ client/src/components/WallPaper.jsx
@@ -8,13 +8,6 @@ const WallPaper = () => {
       <div className='header-content d-flex align-items-center align-self-center justify-content-center flex-column h-100'>
         <h1 className='text-dark text-center '>Food Delivery and More</h1>
         <p>You deserve great food</p>
-        {/* <form>
-          <div className='d-flex justify-content-center flex-wrap mb-3'>
-            <div className='d-flex'>
-              <input type="text" placeholder='Search for dish..' />
-            </div>
-          </div>
-        </form> */}
       </div>
     </div>
   )
